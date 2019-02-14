dc5edd975b534a014d54f121c26db146ac5a988c
Robert Hung Nguyen <nthung2411@gmail.com>
2019-02-14 14:27:40 +0700

    add bash for github page publish

---
+++ github_page_publish.sh
@@ -0,0 +1,13 @@
+echo 'install angular-cli-ghpages'
+npm i angular-cli-ghpages --save
+
+echo 'npm install'
+npm i
+
+echo 'build and publish www to https://nthung2411.github.io/ionic-lazyload-v4/'
+ionic build --prod -- --base-href https://nthung2411.github.io/ionic-lazyload-v4/
+
+echo 'publish branch gh-pages'
+npx angular-cli-ghpages --dir=www
+
+echo 'published finish, please update SETTINGS to use branch gh-pages for github page'
